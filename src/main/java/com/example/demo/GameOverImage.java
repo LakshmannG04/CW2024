f33b6d3598052a9d9d96efe0c8b135f878fe204a
LakshmannG04 <lakshmannganeson@gmail.com>
2024-12-15 15:11:41 +0800

    Adding Javadocs comments

--- src/main/java/com/example/demo/GameOverImage.java
+++ src/main/java/com/example/demo/GameOverImage.java
@@ -3,13 +3,24 @@ package com.example.demo;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * The {@code GameOverImage} class represents the "Game Over" screen image in the game.
+ * It extends the {@link ImageView} class and provides functionality for setting the image
+ * and its position.
+ */
 public class GameOverImage extends ImageView {
 	
+	/** The file path of the "Game Over" image. */
 	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
 
+	/**
+	 * Constructs a new {@code GameOverImage} object and sets its position on the screen.
+	 *
+	 * @param xPosition the X-coordinate position of the image
+	 * @param yPosition the Y-coordinate position of the image
+	 */
 	public GameOverImage(double xPosition, double yPosition) {
-		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
+		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()));
 		setLayoutX(xPosition);
 		setLayoutY(yPosition);
 	}
