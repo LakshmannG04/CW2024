80a5eba21929d55c186ece530c9fa07592d44452
LakshmannG04 <lakshmannganeson@gmail.com>
2024-12-15 15:16:05 +0800

    Adding Javadocs comments

--- src/main/java/com/example/demo/Projectile.java
+++ src/main/java/com/example/demo/Projectile.java
@@ -1,17 +1,45 @@
 package com.example.demo;
 
+import javafx.scene.image.Image;
+
+/**
+ * The Projectile class is an abstract class that represents a projectile in the game. 
+ * It extends the ActiveActorDestructible class, meaning it has properties and behaviors
+ * common to all destructible active actors, such as taking damage and being destroyed.
+ * The Projectile class is designed to be subclassed for different types of projectiles
+ * with specific behaviors for updating their position and appearance.
+ */
 public abstract class Projectile extends ActiveActorDestructible {
 
-	public Projectile(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-		super(imageName, imageHeight, initialXPos, initialYPos);
-	}
+    /**
+     * Constructor for the Projectile class. Initializes the projectile with an image, 
+     * image height, and initial position on the screen.
+     * 
+     * @param imageName The name of the image to be used for the projectile.
+     * @param imageHeight The height of the image (used for scaling).
+     * @param initialXPos The initial X position of the projectile on the screen.
+     * @param initialYPos The initial Y position of the projectile on the screen.
+     */
+    public Projectile(String imageName, int imageHeight, double initialXPos, double initialYPos) {
+        // Calls the constructor of the superclass with the image and position details.
+        super(new Image(Projectile.class.getResourceAsStream("/com/example/demo/images/" + imageName)), 
+              imageHeight, initialXPos, initialYPos);
+    }
 
-	@Override
-	public void takeDamage() {
-		this.destroy();
-	}
+    /**
+     * This method handles the damage interaction for the projectile. 
+     * In this case, the projectile is destroyed when it takes damage.
+     */
+    @Override
+    public void takeDamage() {
+        this.destroy();  // Destroys the projectile
+    }
 
-	@Override
-	public abstract void updatePosition();
-
+    /**
+     * Abstract method to update the position of the projectile. 
+     * This method must be implemented by subclasses to define how 
+     * the projectile's position changes during the game.
+     */
+    @Override
+    public abstract void updatePosition();
 }
